fcbdc69484480c4496faaabb4ab0f9129f055679
JosephHalim <joseph.e.halim@gmail.com>
2014-05-13 08:10:52 -0700

    Trying to Retrieve File Deleted

--- ListApplication/PanoramaApp1/List.xaml.cs
+++ ListApplication/PanoramaApp1/List.xaml.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+
+using System.Windows.Input;
 
 namespace PanoramaApp1
 {
@@ -16,5 +19,32 @@ namespace PanoramaApp1
         {
             InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Title.Text = myGlobals.CurrentList;
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+
+        }
+      private  void Add_New_Item(object sender, EventArgs e)
+        {
+            var popup = new Popup();
+            
+      //  NewItem control = new AddNewControl();
+
+        }
+      private void DeleteList(object sender, EventArgs e)
+        {
+            string hold= "text";
+        }
+     private    void SortData(object sender, EventArgs e)
+      {
+
+      }
+        
     }
+
 }
--- ListApplication/PanoramaApp1/MainPage.xaml.cs
+++ ListApplication/PanoramaApp1/MainPage.xaml.cs
@@ -35,25 +35,30 @@ namespace PanoramaApp1
             if (myGlobals.ListofItems.Count == 0) { }
             else
             {
-                
-              for(int x=0; x<(myGlobals.ListofItems.Count);x++)
-              {
-                  TextBlock ListColumn = new TextBlock();
-                  {
-                  ListColumn.FontSize = 30;
-                  ListColumn.Name = x.ToString();
-                  ListColumn.Text = myGlobals.ListofItems[x];
-                  };
-                  ListColumn.Tap += new EventHandler<GestureEventArgs>(HandleTap);
-                
-                  ListNamePanel.Children.Add(ListColumn);
-                  
-              }
+                //Remove Past TextBlocks
+        //     foreach(TextBlock c in ListNamePanel)
+
+                ListNamePanel.Children.Clear();
+
+                //Add TextBlock
+                    for (int x = 0; x < (myGlobals.ListofItems.Count); x++)
+                    {
+                        TextBlock ListColumn = new TextBlock();
+                        {
+                            ListColumn.FontSize = 30;
+                            ListColumn.Name = x.ToString();
+                            ListColumn.Text = myGlobals.ListofItems[x];
+                        };
+                        ListColumn.Tap += new EventHandler<GestureEventArgs>(HandleTap);
+
+                        ListNamePanel.Children.Add(ListColumn);
+
+                    }
                // tbTemp.Text = myGlobals.ListofItems[0];
             }
      
     }
-
+        
         private void Pivot_Loaded(object sender, RoutedEventArgs e)
         {
 
@@ -71,12 +76,12 @@ namespace PanoramaApp1
        void HandleTap(Object sender, GestureEventArgs e)
         {
 
-            object ListName = sender.GetType();//.GetProperty("Name", typeof(string)).ToString();
+            
          TextBlock tb= new TextBlock();
            tb = (TextBlock)sender;
-           string hold = tb.Text;
-           MessageBox.Show(hold);
-           
+           myGlobals.CurrentList = tb.Text;
+          
+           NavigationService.Navigate(new Uri("/List.xaml", UriKind.Relative));
         }
 
         void HandleHold(object sender, GestureEventArgs e)
