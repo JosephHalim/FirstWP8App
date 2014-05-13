7f52d7659d91810e2ea7d5e414fe600a41e1499c
JosephHalim <joseph.e.halim@gmail.com>
2014-05-12 21:32:45 -0700

    Fixed List
    Updated List, so there's no duplicates, installed Coding4Fun Toolkit.

--- ListApplication/PanoramaApp1/List.xaml.cs
+++ ListApplication/PanoramaApp1/List.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
@@ -16,5 +17,27 @@ namespace PanoramaApp1
         {
             InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Title.Text = myGlobals.CurrentList;
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        void Add_New_Item(object sender, EventArgs e)
+        {
+            var popup = new Popup();
+            
+      //  NewItem control = new AddNewControl();
+
+            
+
+
+
+        }
     }
+
 }
